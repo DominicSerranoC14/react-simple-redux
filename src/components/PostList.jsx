4bae5fd04b480a94d85e9aebcf1243b2d780c01f
Dominic Serrano <dmsrojo@gmail.com>
2017-11-30 23:18:07 -0600

    Removed post from post list

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import PostForm from './PostForm.jsx';
 
 // Display posts
-const Posts = ({ posts }) => {
+const Posts = ({ posts, removePost }) => {
     if (posts.length === 0) {
         return <p>There are currently no posts.</p>
     }
 
-    return posts.map((post, i) => <p key={i}>{post}</p>);
+    return posts.map((post, i) => (
+        <div key={i}>
+            <p>{post}</p>
+            <button onClick={() => removePost(i)}>Delete</button>
+        </div>
+    ));    
 };  
 
 // Handle state and render child components
@@ -25,6 +30,7 @@ class PostList extends Component {
         this.createPost = this.createPost.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.removePost = this.removePost.bind(this);
     }
 
     addPost(post) {
@@ -44,6 +50,12 @@ class PostList extends Component {
         }, 2000);
     }
 
+    removePost(index) {
+        this.setState(prevState => ({
+            posts: prevState.posts.filter((post, i) => i !== index),
+        }));
+    }
+
     handleChange(event) {
         this.setState({ title: event.target.value });
     }
@@ -59,7 +71,7 @@ class PostList extends Component {
             <div>
                 <p>Post List</p>
 
-                <Posts posts={this.state.posts} />
+                <Posts posts={this.state.posts} removePost={this.removePost}/>
 
                 <PostForm createPost={this.createPost}
                     formDisabled={this.state.formDisabled}
